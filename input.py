0c29e5b728194c702d52669af496b3865ff8796c
groomigroom <googjohn0143@gmail.com>
2026-03-18 07:47:24 +0900

    Update input.py

--- input.py
+++ input.py
@@ -21,4 +21,4 @@ print(result)
 userName = input("이름 ==> ")
 userPhone = input("전화번호 ==> ")
 
-print("제 이름은 ", userName, "입니다. 연락처는 ", userPhone, "입니다.)
+print("제 이름은 ", userName, "입니다. 연락처는 ", userPhone, "입니다.")
