ec089944c9b7c424a3204dad264cb0cb1a292d33
groomigroom <googjohn0143@gmail.com>
2026-03-18 07:40:04 +0900

    Update input.py

--- input.py
+++ input.py
@@ -2,4 +2,11 @@ num_one = input()
 num_two = input("숫자 ==> ")
 print(num_one + num_two)
 
-#결과는 12
+#
+
+num_one = input()
+num_two = input("숫자 ==> ")
+result = num_one + num_two
+print(result)
+
+#두개의 결과는 두 수 받은거 붙인거로 나옴
