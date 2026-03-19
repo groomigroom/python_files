9a294fe388f6ddb5681678f1e087b7858f5654fe
groomigroom <googjohn0143@gmail.com>
2026-03-20 07:06:49 +0900

    Create 84쪽 정보처리기사_dictionary.py

---
+++ 84쪽 정보처리기사_dictionary.py
@@ -0,0 +1,8 @@
+diction_a = {'이름':'김구름', '나이':9, '주소':'강아지집'}
+diction_b = dict({'이름':'김구름', '나이':9, '주소':'강아지집'})
+
+print(diction_a, diction_b)
+
+diction_a['이름'] = '김구름이'
+
+print(diction_a, diction_b)
