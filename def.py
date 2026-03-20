461f6408f9d4990682e235ac24ba57aab0160faf
groomigroom <googjohn0143@gmail.com>
2026-03-20 09:20:54 +0900

    Update def.py

--- def.py
+++ def.py
@@ -8,3 +8,18 @@ def is_even(num):
 print (is_even(1))
 print (is_even(4))
 print (is_even(7))
+
+#####
+
+def calc(x, y):
+	
+	x *= 3
+	y /= 3
+	print(x, y)
+	return x
+
+a, b = 3, 12
+a = calc(a, b)
+
+print(a, b)
+
