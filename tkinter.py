18b1992d4fd9fd0d3e5cd83dacabfb893e4d4688
groomigroom <googjohn0143@gmail.com>
2026-03-26 17:48:22 +0900

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,4 +1,15 @@
 from tkinter import *
+from tkinter import messagebox
+
+def myFunc() :
+    messagebox.showinfo("경고", "주의하세요")
+
+def myChec():
+    if chk.get() == 0 :
+        messagebox.showinfo("체크체크", "체크가 안되었어요.....")
+
+    else :
+        messagebox.showinfo("체크체크", "체크되었네요~~~!!!!")
 
 root = Tk()
 
@@ -12,6 +23,13 @@ label2 = Label(root, text="열심히 공부하세요", font=("pretendard", 25), 
 label2.pack()
 label3 = Label(root, text="생일축하해요", font=("pretendard", 25), bg="#FFFFFF", fg="#333333", anchor=CENTER)
 label3.pack()
+
+button1 = Button(root, text="클릭", fg="#333333", command=myFunc)
+button1.pack()
+
+chk = IntVar()
+cb1 = Checkbutton(root, text="체크", variable=chk, command=myChec)
+cb1.pack()
 
 root.mainloop()
 
