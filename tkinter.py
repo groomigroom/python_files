74e91393828e471fba1b4f17dd84276f81d297fd
groomigroom <googjohn0143@gmail.com>
2026-03-26 17:03:42 +0900

    Create tkinter.py

---
+++ tkinter.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("창 조절 연습")
+root.geometry("500x200")
+root.resizable(width=FALSE, height=FALSE)
+
+label1 = Label(root, text="")
+label1.pack()
+label2 = Label(root, text="열심히 공부하세요", font=("pretendard", 25), fg="green")
+label2.pack()
+label3 = Label(root, text="형주는 생일축하해요", font=("pretendard", 25), bg="#FFFFFF", fg="#333333", anchor=CENTER)
+label3.pack()
+
+root.mainloop()
+
+#width=20, height=3, 
